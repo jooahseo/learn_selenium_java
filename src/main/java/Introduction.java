7aa59aac95d4005ca5392b991e2ddf3372f36331
jooahseo <jooah.seo@radian.com>
2021-11-07 19:13:22 -0500

    updat to maven project

---
+++ src/main/java/Introduction.java
@@ -0,0 +1,7 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+    }
+}

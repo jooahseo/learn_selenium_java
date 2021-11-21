ac48c0eabecd9ccff4286409f08f29cf7345807c
Jooah Seo <seo.jooah@gmail.com>
2021-11-21 12:40:41 -0500

    webdriver setup basic

--- src/main/java/Introduction.java
+++ src/main/java/Introduction.java
@@ -1,7 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Launch the website using webdriver and close.
+ */
 public class Introduction {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver_win32\\chromedriver.exe");
+        //windows
+        //System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver_win32\\chromedriver.exe");
+        //mac
+        System.setProperty("webdriver.chrome.driver", "/Users/jooahseo/work/chromedriver");
 
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+
+        driver.close();
     }
 }

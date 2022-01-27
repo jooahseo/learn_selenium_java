065e8cd77e60c2fe19ca04366bebe572ec9f2635
Jooah Seo <seo.jooah@gmail.com>
2022-01-26 22:25:57 -0500

    basic log method

--- src/main/java/Log4jDemo.java
+++ src/main/java/Log4jDemo.java
@@ -5,6 +5,9 @@ public class Log4jDemo {
     public static Logger log = LogManager.getLogger(Log4jDemo.class.getName());
 
     public static void main(String[] args) {
-
+        log.debug("debug message");
+        log.info("info message");
+        log.error("error message");
+        log.fatal("fatal message");
     }
 }

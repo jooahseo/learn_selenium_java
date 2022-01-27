56ac54536741a1bc0652ec0a2922f2633318cf36
Jooah Seo <seo.jooah@gmail.com>
2022-01-26 21:46:48 -0500

    add Log4jDemo

---
+++ src/main/java/Log4jDemo.java
@@ -0,0 +1,10 @@
+import org.apache.logging.log4j.*;
+
+public class Log4jDemo {
+
+    public static Logger log = LogManager.getLogger(Log4jDemo.class.getName());
+
+    public static void main(String[] args) {
+
+    }
+}
